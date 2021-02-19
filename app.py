f3b9842cd4e82ff9805fe37114c11f1560652a9c
dataininstitute <68183539+dataininstitute@users.noreply.github.com>
2021-02-19 16:13:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,31 +19,31 @@ def run():
     st.image(image,use_column_width=False)
 
     add_selectbox = st.sidebar.selectbox(
-    "How would you like to predict?",
+    "Comment aimeriez-vous effectuer vos prédictions?",
     ("Online", "Batch"))
 
-    st.sidebar.info('This app is created to predict patient hospital charges')
+    st.sidebar.info('Cette application est créée pour prédire les frais d"hospitalisation des patients')
     st.sidebar.success('https://www.pycaret.org')
     
     st.sidebar.image(image_hospital)
 
-    st.title("Insurance Charges Prediction App")
+    st.title("Application de prévision des frais d'assurance")
 
     if add_selectbox == 'Online':
 
         age = st.number_input('Age', min_value=1, max_value=100, value=25)
-        sex = st.selectbox('Sex', ['male', 'female'])
-        bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
-        children = st.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
-        if st.checkbox('Smoker'):
-            smoker = 'yes'
+        sex = st.selectbox('Sex', ['Homme', 'Femme'])
+        bmi = st.number_input('IMC', min_value=10, max_value=50, value=10)
+        children = st.selectbox('Enfants', [0,1,2,3,4,5,6,7,8,9,10])
+        if st.checkbox('Fumeur'):
+            smoker = 'oui'
         else:
-            smoker = 'no'
-        region = st.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
+            smoker = 'non'
+        region = st.selectbox('Région', ['Sud Ouest', 'Nord Ouest', 'Nord Est', 'Sud Est'])
 
         output=""
 
-        input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
+        input_dict = {'age' : age, 'sexe' : sex, 'imc' : bmi, 'enfant' : children, 'fummeur' : smoker, 'région' : region}
         input_df = pd.DataFrame([input_dict])
 
         if st.button("Predict"):
@@ -54,7 +54,7 @@ def run():
 
     if add_selectbox == 'Batch':
 
-        file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+        file_upload = st.file_uploader("Uploader votre fichier csv pour effectuer vos prédicitons", type=["csv"])
 
         if file_upload is not None:
             data = pd.read_csv(file_upload)
